92e053aa78957d5912523115ae38e45b6d31ad33
Pritam Anand <pritamanand60@gmail.com>
2026-09-11 10:39:55 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,40 +1,28 @@
 import { createContext, useState, useEffect } from "react";
-
 export const AuthContext = createContext();
-
 export function AuthProvider({ children }) {
 
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
     const saved = localStorage.getItem("user");
-
     if (saved)
       setUser(JSON.parse(saved));
-
     setLoading(false);
-
   }, []);
 
   const login = (data) => {
-
     localStorage.setItem(
       "user",
       JSON.stringify(data)
     );
-
     setUser(data);
   };
-
   const logout = () => {
-
     localStorage.removeItem("user");
-
     setUser(null);
   };
-
   return (
 
     <AuthContext.Provider
@@ -45,11 +33,7 @@ export function AuthProvider({ children }) {
         loading
       }}
     >
-
       {children}
-
     </AuthContext.Provider>
-
   );
-
 }
