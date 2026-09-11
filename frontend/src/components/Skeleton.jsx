c9fe052cbafd566b32b44367a95a146d5fde1c26
Pritam Anand <pritamanand60@gmail.com>
2026-09-11 10:50:33 +0530

    Update Skeleton.jsx

--- frontend/src/components/Skeleton.jsx
+++ frontend/src/components/Skeleton.jsx
@@ -1,19 +1,10 @@
 import React from "react";
-
 export default function Skeleton() {
-
   return (
-
     <div className="skeleton-card">
-
       <div className="skeleton-img"></div>
-
       <div className="skeleton-text"></div>
-
       <div className="skeleton-text short"></div>
-
     </div>
-
   );
-
 }
